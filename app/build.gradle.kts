14d6370e61dc96f552d3d19432f1288388fdce3b
moka <aud1220a@gmail.com>
2021-10-10 08:42:04 +0900

    v1.0.2 (3)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "be.dist.notiflow"
         minSdk = 28
         targetSdk = 30
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.2"
     }
     buildFeatures {
         viewBinding = true
